41b807cf279cbb90a8105270243877bf7de92474
Demian Ezequiel Bonafina <102964042+dbonafin@users.noreply.github.com>
2022-09-29 17:55:59 +0200

    sandbox payment added + checkout page started

---
+++ braintreeTutorial/routes/checkout.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const braintree = require('braintree');
+
+router.post('/', (req, res, next) => {
+  const gateway = new braintree.BraintreeGateway({
+    environment: braintree.Environment.Sandbox,
+    // Use your own credentials from the sandbox Control Panel here
+    merchantId: 'wkm3qxg9ks6mtzhc',
+    publicKey: 'dtfmtmk469h5fspq',
+    privateKey: '7bc596e207fb2bd82386489c7e0c0711'
+  });
+
+  // Use the payment method nonce here
+  const nonceFromTheClient = req.body.paymentMethodNonce;
+  // Create a new transaction for $10
+  const newTransaction = gateway.transaction.sale({
+    amount: '10.00',
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      // This option requests the funds from the transaction
+      // once it has been authorized successfully
+      submitForSettlement: true
+    }
+  }, (error, result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(500).send(error);
+      }
+  });
+});
+
+module.exports = router;
