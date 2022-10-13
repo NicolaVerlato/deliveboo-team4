71bbdefbdeff946abeac0ddedd6b25e72921f1a4
E-Lop <edmondolopez@gmail.com>
2022-10-13 17:57:18 +0200

    website title

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Document</title>
+    <title>Deliveboo</title>
 </head>
 <body style="background-color: #fea543">
     <div id="root"></div>
